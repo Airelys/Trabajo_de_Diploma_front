cd51e981e91b63065ca2a12fc6a15001c815d92a
Airelys <Airelys@DESKTOP-PNPOLM3>
2022-10-31 01:39:01 -0400

    mejorando interfaz

--- src/app/components/initialize-model/initialize-model.component.ts
+++ src/app/components/initialize-model/initialize-model.component.ts
@@ -13,30 +13,28 @@ import { FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
 })
 export class InitializeModelComponent implements OnInit {
 
-  subscription: Subscription ;
-  model_name: ModelName = new ModelName();
   form:FormGroup;
   form2:FormGroup;
-  beta_est:Boolean =true
+  subscription: Subscription = new Subscription();
+  model_name: ModelName = new ModelName();
+  vars_initials = [{},{},{},{}]
+  params_initials = [{},{},{},{},{},{},{}]
+  params_est = [{},{},{},{},{},{},{}]
+  params_int = [{},{},{},{},{},{},{}]
+  params_max = [{},{},{},{},{},{},{}]
+  params_min = [{},{},{},{},{},{},{}]
   methods = [{name:'RK45'},{name:'RK23'},{name:'DOP853'},{name:'Radau'},{name:'BDF'},{name:'LSODA'}]
   classical_methods = [{name:'CG'},{name:'Newton-CG'},{name:'BFGS'},{name:'L-BFGS-B'}]
   metaheuristics = [{name:'PSO'},{name:'DE'}]
 
-  constructor(private route: ActivatedRoute, private modelService:SolveModelService, private fb: FormBuilder) { 
-    
+  constructor(private route: ActivatedRoute, private modelService:SolveModelService, private fb: FormBuilder) {
+
     this.subscription=this.modelService.obtModelName().subscribe(data => {
       console.log(data)
     })
 
     this.form = this.fb.group({
       method: [''],
-      b_val: [Number], b_est: [Boolean], b_int: [Boolean], b_max: [Number], b_min: [Number],
-      g_val: [Number], g_est: [Boolean], g_int: [Boolean], g_max: [Number], g_min: [Number],
-      d_val: [Number], d_est: [Boolean], d_int: [Boolean], d_max: [Number], d_min: [Number],
-      e_val: [Number], e_est: [Boolean], e_int: [Boolean], e_max: [Number], e_min: [Number],
-      l_val: [Number], l_est: [Boolean], l_int: [Boolean], l_max: [Number], l_min: [Number],
-      m_val: [Number], m_est: [Boolean], m_int: [Boolean], m_max: [Number], m_min: [Number],
-      mi_val: [Number], mi_est: [Boolean], mi_int: [Boolean], mi_max: [Number], mi_min: [Number],
       S:[Number],I:[Number],R:[Number],E:[Number],
       t:[Number],
       total_points:[Number]
@@ -58,6 +56,64 @@ export class InitializeModelComponent implements OnInit {
       console.log(data)
       this.model_name = data
     })
+  }
+
+  updateParamsInitials(position:number,value:string):void{
+    this.params_initials.splice(position,1,Number(value))
+  }
+
+  updateVarsInitials(position:number,value:string):void{
+    this.vars_initials.splice(position,1,Number(value))
+  }
+
+  updateParamsEstInitials(position:number,value:string):void{
+    this.params_est.splice(position,1,Boolean(value))
+  }
+
+  updateParamsMinInitials(position:number,value:string):void{
+    this.params_min.splice(position,1,Number(value))
+  }
+
+  updateParamsMaxInitials(position:number,value:string):void{
+    this.params_max.splice(position,1,Number(value))
+  }
+
+  updateParamsIntInitials(position:number,value:string):void{
+    this.params_int.splice(position,1,Boolean(value))
+  }
+
+  updateModelData():void{
+    var temp = [this.params_initials, this.params_est,this.params_int,this.params_min,this.params_max]
+
+    switch(this.model_name.model_name){
+      case 'SI':
+        for (let element of temp){
+          element= [element[0],element[4],element[5],element[6]];
+        }
+        this.vars_initials = [this.vars_initials[0],this.vars_initials[1]];
+        break;
+      case 'SIR':
+        for (let element of temp){
+          element= [element[0],element[1],element[4],element[5],element[6]];
+        }
+        this.vars_initials = [this.vars_initials[0],this.vars_initials[1],this.vars_initials[2]];
+        break;
+      case 'SIRS':
+        for (let element of temp){
+          element= [element[0],element[1],element[2],element[4],element[5],element[6]];
+        }
+        this.vars_initials = [this.vars_initials[0],this.vars_initials[1],this.vars_initials[2]];
+        break;
+        break;
+      case 'SEIR':
+        for (let element of temp){
+          element= [element[0],element[3],element[1],element[4],element[5],element[6]];
+        }
+        this.vars_initials = [this.vars_initials[0],this.vars_initials[1],this.vars_initials[2],this.vars_initials[3]];
+        break;
+      default:
+        break
+    }
   }
 
 }
